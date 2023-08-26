2c2a25e019a294861af6ea69c35d5ee62041b6c8
Kevin-Mwole <mwole100@gmail.com>
2023-08-26 17:48:08 +0300

    get basic test working

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@components(.*)$': '<rootDir>/components$1',
+    '^@context(.*)$': '<rootDir>/context$1',
+    '^@pages(.*)$': '<rootDir>/pages$1',
+    '^@icons(.*)$': '<rootDir>/icons$1',
+    '^@features(.*)$': '<rootDir>/features$1',
+    '^@services(.*)$': '<rootDir>/services$1',
+    '^@utils(.*)$': '<rootDir>/utils$1',
+    '\\.(jpg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+}
