883a7d1cd3bc3f025445b99fd556363ad73cf8b2
Kevin-Mwole <mwole100@gmail.com>
2023-08-26 17:44:29 +0300

    remove logging

---
+++ services/client.ts
@@ -0,0 +1,32 @@
+type ClientOptions<TData extends Object> = RequestInit & {
+  data?: TData
+}
+
+export async function client<TResult = unknown, TData = {}>(
+  endpoint: string,
+  { data, headers: customHeaders, ...initOptions }: ClientOptions<TData> = {}
+): Promise<TResult> {
+  const headers: HeadersInit = {}
+  if (data) {
+    headers['content-type'] = 'application/json'
+  }
+
+  const init: RequestInit = {
+    method: data ? 'POST' : 'GET',
+    body: data && JSON.stringify(data),
+    headers: {
+      ...headers,
+      ...customHeaders,
+    },
+    ...initOptions,
+  }
+
+  try {
+    const res = await fetch(endpoint, init)
+    const result = (await res.json()) as unknown
+    if (!res.ok) return Promise.reject(result)
+    return result as TResult
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
